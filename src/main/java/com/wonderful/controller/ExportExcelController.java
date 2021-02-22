9f53246bc3a28dec8568036513350f51d5b01bfb
ZJ <1148916119@qq.com>
2021-02-22 14:59:42 +0800

    导出fix 关于base64编码的一些问题  以及mybatisplus默认分页是500调整为无限制

--- src/main/java/com/wonderful/controller/ExportExcelController.java
+++ src/main/java/com/wonderful/controller/ExportExcelController.java
@@ -9,9 +9,11 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.wonderful.bean.dto.*;
 import com.wonderful.bean.entity.AttacherCardPointDetails;
 import com.wonderful.bean.entity.AttacherCardSaleComparisonDetails;
+import com.wonderful.bean.entity.CustomerInfo;
 import com.wonderful.bean.entity.MasterCardPurchaseDetails;
 import com.wonderful.service.AttacherCardPointDetailsService;
 import com.wonderful.service.AttacherCardSaleComparisonDetailsService;
+import com.wonderful.service.CustomerInfoService;
 import com.wonderful.service.MasterCardPurchaseDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Base64;
@@ -38,6 +41,8 @@ public class ExportExcelController {
     private AttacherCardPointDetailsService attacherCardPointDetailsService;
     @Autowired
     private AttacherCardSaleComparisonDetailsService attacherCardSaleComparisonDetailsService;
+    @Autowired
+    private CustomerInfoService customerInfoService;
 
     @GetMapping(value = "/masterCardPurchaseDetailsExport")
     public String masterCardPurchaseDetailsExport(@RequestParam(value = "encrypted") String encrypted, HttpServletResponse response) throws UnsupportedEncodingException {
@@ -55,7 +60,10 @@ public class ExportExcelController {
         response.setContentType("multipart/form-data");
         try {
             //获取要导出的数据
-            String decrypted = new String(Base64.getUrlDecoder().decode(encrypted.getBytes()));
+            //将空格替换为+，
+            String str = encrypted.replaceAll(" +","+");
+            //回车换行去掉
+            String decrypted = new String(Base64.getDecoder().decode(str.replace("\r\n", "")),"utf-8");
             MasterCardPurchaseDetailsDTO masterCardPurchaseDetailsDTO = JSON.parseObject(decrypted, MasterCardPurchaseDetailsDTO.class);
             IPage<MasterCardPurchaseDetails> page = masterCardPurchaseDetailsService.page(masterCardPurchaseDetailsDTO);
             List<MasterCardPurchaseDetails> records = page.getRecords();
@@ -99,7 +107,10 @@ public class ExportExcelController {
         response.setContentType("multipart/form-data");
         try {
             //获取要导出的数据
-            String decrypted = new String(Base64.getUrlDecoder().decode(encrypted.getBytes()));
+            //将空格替换为+，
+            String str = encrypted.replaceAll(" +","+");
+            //回车换行去掉
+            String decrypted = new String(Base64.getDecoder().decode(str.replace("\r\n", "")),"utf-8");
             AttacherCardPointDetailsDTO attacherCardPointDetailsDTO = JSON.parseObject(decrypted, AttacherCardPointDetailsDTO.class);
             IPage<AttacherCardPointDetails> page = attacherCardPointDetailsService.page(attacherCardPointDetailsDTO);
             List<AttacherCardPointDetails> records = page.getRecords();
@@ -143,7 +154,10 @@ public class ExportExcelController {
         response.setContentType("multipart/form-data");
         try {
             //获取要导出的数据
-            String decrypted = new String(Base64.getUrlDecoder().decode(encrypted.getBytes()));
+            //将空格替换为+，
+            String str = encrypted.replaceAll(" +","+");
+            //回车换行去掉
+            String decrypted = new String(Base64.getDecoder().decode(str.replace("\r\n", "")),"utf-8");
             AttacherCardSaleComparisonDetailsDTO attacherCardSaleComparisonDetailsDTO = JSON.parseObject(decrypted, AttacherCardSaleComparisonDetailsDTO.class);
             IPage<AttacherCardSaleComparisonDetails> page = attacherCardSaleComparisonDetailsService.page(attacherCardSaleComparisonDetailsDTO);
             List<AttacherCardSaleComparisonDetails> records = page.getRecords();
@@ -153,6 +167,53 @@ public class ExportExcelController {
             List<AttacherCardSaleComparisonDetailsExportDTO> content = records.stream().map(o -> {
                 AttacherCardSaleComparisonDetailsExportDTO attacherCardSaleComparisonDetailsExportDTO = BeanUtil.toBean(o, AttacherCardSaleComparisonDetailsExportDTO.class);
                 return attacherCardSaleComparisonDetailsExportDTO;
+            }).collect(Collectors.toList());
+
+            //导出
+            ServletOutputStream out = response.getOutputStream();
+            ExcelWriter writer = new ExcelWriter(out, ExcelTypeEnum.XLSX, true);
+            writer.write(content, sheet);
+            writer.finish();
+            out.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "导出失败";
+        } finally {
+            //导出流结束，计数归0
+            exportCount.set(0);
+        }
+        return "导出成功";
+    }
+
+    @GetMapping(value = "/customerInfoExport")
+    public String customerInfoExport(@RequestParam(value = "encrypted") String encrypted, HttpServletResponse response) throws UnsupportedEncodingException {
+        //AtomicInteger原子操作类，导出流输出完之前不允许多次调用点击
+        if (exportCount.incrementAndGet() > 1) {
+            return "导出失败，重复导出";
+        }
+        Sheet sheet = new Sheet(1, 0, CustomerInfoExportDTO.class);
+        //设置自适应宽度
+        sheet.setAutoWidth(Boolean.TRUE);
+        String fileName = String.format("客户信息%s", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss")));
+        //通知浏览器以附件的形式下载处理
+        response.setCharacterEncoding("utf-8");
+        response.setHeader("Content-disposition", "attachment;filename=" + new String(fileName.getBytes("utf-8"),"iso-8859-1") + ".xlsx");
+        response.setContentType("multipart/form-data");
+        try {
+            //获取要导出的数据
+            //将空格替换为+，
+            String str = encrypted.replaceAll(" +","+");
+            //回车换行去掉
+            String decrypted = new String(Base64.getDecoder().decode(str.replace("\r\n", "")),"utf-8");
+            CustomerInfoDTO customerInfoDTO = JSON.parseObject(decrypted, CustomerInfoDTO.class);
+            IPage<CustomerInfo> page = customerInfoService.page(customerInfoDTO);
+            List<CustomerInfo> records = page.getRecords();
+            if (records.size() == 0) {
+                return "暂无数据导出";
+            }
+            List<CustomerInfoExportDTO> content = records.stream().map(o -> {
+                CustomerInfoExportDTO customerInfoExportDTO = BeanUtil.toBean(o, CustomerInfoExportDTO.class);
+                return customerInfoExportDTO;
             }).collect(Collectors.toList());
 
             //导出
